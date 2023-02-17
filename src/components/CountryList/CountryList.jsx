8f0c73da03e84dbd3c6ab6a73f7d70fbffe03329
Alexander Kushnir <dj.egregor1@gmail.com>
2023-02-17 20:59:39 +0100

    country info

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,12 +1,12 @@
-import { Grid, GridItem } from "components";
-import { Link } from "react-router-dom";
+import { Grid, GridItem } from 'components';
+import { Link } from 'react-router-dom';
 
 export const CountryList = ({ countries }) => {
   return (
     <Grid>
       {countries.map(({ id, country, flag }) => {
         return (
-          <GridItem>
+          <GridItem key={id}>
             <Link to={`/country/${id}`}>
               <img src={flag} alt={country} />
             </Link>
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,23 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
+import { useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/country-service';
+import { useEffect, useState } from 'react';
 
 export const Country = () => {
+  const { id } = useParams();
+  const [country, setCountry] = useState({});
+
+  useEffect(() => {
+    fetchCountry(id).then((country) => {
+      setCountry(country);
+      console.log(country);
+    });
+  }, [id]);
+
   return (
     <Section>
       <Container>
-        <h2>Country</h2>
+        <CountryInfo flag={country.flag} capital={country.capital} country={country.countryName} id={country.id} languages={country.languages} population={country.population} />
       </Container>
     </Section>
   );
